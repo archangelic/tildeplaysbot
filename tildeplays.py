2d2fcfe8ec98de21c524cbbb1914cf966b83038e
MineRobber___T <MineRobber9000@users.noreply.github.com>
2017-08-22 15:12:57 -0400

    Add shutdown command and fix error crashes.
    This PR will add a shutdown command usable by the operator of the bot. It also prevents errors from crashing the bot, instead writing errors to a file and notifying the operator to check the log and file an issue so that they (or someone like me) can fix it.

--- tildeplays.py
+++ tildeplays.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from os import environ, path
 from textwrap import wrap
-import time
-import subprocess
+import time,subprocess,launch,traceback
 
 from pykeyboard import PyKeyboard
 
@@ -17,6 +16,8 @@ cmds = {'up': k.up_key,
         'select': k.shift_key,
         'save': k.function_keys[5],
         'reload': k.function_keys[7]}
+
+active = False
 
 # clear the screen
 print('\n' * 100)
@@ -33,6 +34,11 @@ def tp_logger(nick, text):
         for i in output:
             chat.write(i + '\n')
 
+def errorlog(e):
+    with open("error.log","a") as f:
+        f.write(time.strftime('[%Y-%m-%d %H:%M:%S] '))
+        f.write(str(e)+"\n")
+        traceback.print_exception(file=f)
 
 def presskey(key):
     k.press_key(key)
@@ -41,16 +47,29 @@ def presskey(key):
 
 
 def run(nick, text):
-    subprocess.call('xdotool search --name "FCEUX 2.2.2" windowactivate', shell=True)
-    if text.startswith(('tildeplaysbot:', '!ff')):
-        text = ''.join([i + ' ' for i in text.split(' ')[1:]]).strip()
-        tp_logger(nick, text)
-    if text == 'power':
-        game = path.join(environ['HOME'], 'Final Fantasy (USA).nes')
-        subprocess.call('fceux "{}" &'.format(game), shell=True)
-        subprocess.call('obs --startstreaming &', shell=True)
-    if text in cmds:
-        tp_logger(nick, text)
-        presskey(cmds[text])
-    else:
-        pass
+    try:
+        subprocess.call('xdotool search --name "FCEUX 2.2.2" windowactivate', shell=True)
+        if text.startswith(('tildeplaysbot:', '!ff')):
+            text = ''.join([i + ' ' for i in text.split(' ')[1:]]).strip()
+            tp_logger(nick, text)
+        if text == 'power':
+            game = path.join(environ['HOME'], 'Final Fantasy (USA).nes')
+            launch.launch('game','fceux "{}" &'.format(game), shell=True)
+            launch.launch('stream','obs --startstreaming &', shell=True)
+            active = True
+        if text == 'shutdown' and nick == op:
+            launch.kill('stream') # kill stream before game
+            launch.kill('game')
+            active = False
+        if text == '!games':
+            return 'Play along with us by typing up, down, left, right, a, b, start, select, save, or reload. Watch at twitch.tv/tildeplays'
+        if text in cmds:
+            if not active:
+                return 'Make sure to power on the bot first! (power)'
+            tp_logger(nick, text)
+            presskey(cmds[text])
+       else:
+           pass
+    except Exception as e:
+        errorlog(e)
+        return 'An error occurred. {} should check the error log and file an issue at github.com/archangelic/tildeplays.'.format(op)
